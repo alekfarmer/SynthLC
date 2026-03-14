e7148c5cbbf117458d14cd06523a8b99104d02f0
Alek Farmer <afarmer8@stanford.edu>
2026-03-14 16:49:51 -0700

    Added time to out file

--- fv/RUN_JG.sh
+++ fv/RUN_JG.sh
@@ -219,7 +219,7 @@ PROJ="${JOB}/${filename}_jgsession_$DATE"
 if [ "$PONO" -eq "1" ]; then
     $YOSYS_BIN -s $YOSYSF
     for p in $(seq 0 39); do
-        time $PONO_BIN --witness -p ${p} -v 5 ${JOB}/${filename}.btor2 > ${JOB}/out/prop${p}.out 2>&1
+        { time $PONO_BIN --witness -p ${p} -v 5 ${JOB}/${filename}.btor2 2>&1 ; } > ${JOB}/out/prop${p}.out 2>&1
     done
 else
     if [ "$gui" -eq "0" ]; then
